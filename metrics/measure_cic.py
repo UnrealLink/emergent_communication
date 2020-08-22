22c5c9a9bb41dac1d4be14c6e39a4e3aaf22b73e
UnrealLink <v.villecroze@gmail.com>
2020-08-22 08:52:33 +0200

    add cic measure

---
+++ metrics/measure_cic.py
@@ -0,0 +1,170 @@
+"""
+Script to measure instantaneous coordination from a trained model from A3C.
+"""
+
+import os
+import argparse
+import logging
+import random
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+
+from social_dilemmas.envs.cleanup import CleanupEnv
+from social_dilemmas.envs.harvest import HarvestEnv
+from social_dilemmas.envs.finder import FinderEnv
+from social_dilemmas.envs.treasure import TreasureEnv
+from social_dilemmas.envs.target import TargetEnv
+
+import utils.utility_funcs as utility_funcs
+
+from algorithms.a3c import ListenerPolicy, SpeakerPolicy, preprocess_messages, preprocess_obs, get_comm
+
+env_map = {
+    'finder': FinderEnv,
+    'treasure': TreasureEnv,
+    'harvest': HarvestEnv,
+    'cleanup': CleanupEnv,
+    'target': TargetEnv,
+}
+
+def get_args():
+    """
+    Get arguments
+    """
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('--env', default='target', type=str, help='environment name')
+    parser.add_argument('--agents', default=1, type=int, help='number of agents in environment')
+    parser.add_argument('--horizon', default=1000, type=int, help='number of steps to measure on')
+    parser.add_argument('--seed', default=0, type=int, help='set random seed')
+    parser.add_argument('--vocab', default=5, type=int, help='vocabulary size for communication')
+    parser.add_argument('--view-size', default=0, type=int, help='view size of agents (0 takes env default)')
+    parser.add_argument('--hidden', default=64, type=int, help='hidden size of GRU')
+    parser.add_argument('--noise', default=0., type=float, help='noise in comm channel')
+    parser.add_argument('--save', default=None, type=str, help='save directory name')
+    parser.add_argument('--checkpoint', default=None, type=int, help='checkpoint to load in save dir (default max)')
+    parser.add_argument('--cpu-only', default=False, action='store_true', help='prevent gpu usage')
+    parser.add_argument('--render', default=False, action='store_true', help='render at each time step')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    if args.save is None:
+        args.save_dir = os.path.join(dir_path, f'saves/{args.env}')
+    else:
+        args.save_dir = os.path.join(dir_path, f'saves/{args.save}')
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    args.communication = True
+
+    if args.cpu_only:
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if args.view_size == 0:
+        args.view_size = None
+
+    logger = logging.getLogger('Measure')
+    utility_funcs.setup_logger(logger, args)
+
+    logger.info("Loading env and agents...")
+    env = env_map[args.env](num_agents=args.agents, seed=args.seed, view_size=args.view_size)
+    torch.manual_seed(args.seed)
+    args.num_actions = env.action_space.n
+
+    models = {
+        'agent-0': ListenerPolicy(channels=3,
+                             memsize=args.hidden,
+                             num_actions=args.num_actions,
+                             vocab_size=args.vocab,
+                             n_agents=args.agents).share_memory().to(device),
+        'agent-1': SpeakerPolicy(channels=3,
+                             memsize=args.hidden,
+                             vocab_size=args.vocab).share_memory().to(device)
+    }
+
+    for agent_name, model in models.items():
+        if model.try_load(args.save_dir, agent_name, logger, checkpoint=args.checkpoint) == 0:
+            logger.warning("No trained models found.")
+        model.eval()
+
+    # Matrices for tracking messages/actions co-occurences
+    CIC = 0.
+
+    logger.info("Starting rollout...")
+    obs = env.reset()
+    states = {
+        agent_name: preprocess_obs(ob, device=device)
+        for agent_name, ob in obs.items()
+    }
+
+    for _ in range(args.horizon):
+        actions = {}
+        messages = {}
+
+       # get speaker message
+        comm_value, comm_logp = models['agent-1'](states['agent-1'])
+
+        message = torch.exp(comm_logp).multinomial(num_samples=1).data[0]
+        messages['agent-1'] = message.cpu().numpy()[0]
+
+        actions['agent-1'] = 4 #STAY
+
+        transmission = preprocess_messages(messages, args.vocab, device=device)
+
+        # select listener action
+        value, logp = models['agent-0']((states['agent-0'], transmission))
+        action = torch.exp(logp).multinomial(num_samples=1).data[0]
+        actions['agent-0'] = action.cpu().numpy()[0]
+
+        # Update measures here
+        pam, pa, pm = 0., 0., 0.
+        pams, pms = [], []
+        for m in range(args.vocab):
+            pm = torch.exp(comm_logp[0][m]).detach().cpu().numpy()
+            fake_messages = {'agent-1' : torch.tensor([m]).to(device)}
+            fake_transmission = preprocess_messages(fake_messages, args.vocab, device=device)
+            fake_value, fake_logp = models['agent-0']((states['agent-0'], fake_transmission))
+            pam = torch.exp(fake_logp[0][action]).detach().cpu().numpy() * pm
+            pa += pam
+            pams.append(pam)
+            pms.append(pm)
+        for pam, pm in zip(pams, pms):
+            if pam * pa * pm != 0:
+                CIC += pam * np.log(pam / (pa * pm)) / args.horizon
+            else:
+                logger.debug('zero value in probas')
+
+
+        obs, rewards, dones, _ = env.step(actions)
+
+        states = {
+            agent_name: preprocess_obs(ob, device=device)
+            for agent_name, ob in obs.items()
+        }
+
+        if args.render:
+            env.render()
+            time.sleep(0.5)
+
+        if dones["__all__"]:
+            obs = env.reset()
+            states = {
+                agent_name: preprocess_obs(ob, device=device)
+                for agent_name, ob in obs.items()
+            }
+            messages = {
+                agent_name: 0
+                for agent_name in models.keys()
+            }
+
+    logger.info("Rollout done.")
+    logger.info("Computing measures")
+
+    logger.info(f"CIC: {CIC}")
